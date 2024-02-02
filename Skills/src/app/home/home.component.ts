9db9dc5750604c151ec3939a8f4132f5222d1ef4
Zacknetic <zackavino@gmail.com>
2024-02-02 11:26:25 -0500

    initial forms implementation

--- Skills/src/app/home/home.component.ts
+++ Skills/src/app/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  customerName: string = "zack";
+  customerEmail: string = "";
+  customerPhone: string = "";
 
+  constructor() { }
+
+  onSubmit(form: FormsModule) {
+    console.log(form);
+  }
+
+  ngOnInit(): void {
+      
+  }
 }

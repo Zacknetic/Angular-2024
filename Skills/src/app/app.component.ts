26c55f38a4b14e2ef2e3529b6e2c522f3a5c1cbe
Zacknetic <zackavino@gmail.com>
2024-02-02 10:53:18 -0500

    removed products, added boilerplate for home

--- Skills/src/app/app.component.ts
+++ Skills/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ProductsComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

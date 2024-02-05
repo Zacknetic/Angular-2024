dafb0cdde365f98db76b7940756621e9a23346f0
Zacknetic <zackavino@gmail.com>
2024-02-05 15:24:33 -0500

    ngFor example

--- Admin-Example/src/app/products/products.component.ts
+++ Admin-Example/src/app/products/products.component.ts
@@ -1,12 +1,14 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products = ["GPU", "CPU", "Memory", "Motherboard", "Storage", "Power Supply", "Case", "Cooling"]
 
 }
